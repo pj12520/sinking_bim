6a963a76cfdd1cd80e2ac7b4968c3507b8a404b2
pj12520 <paul.jarvis@bristol.ac.uk>
2015-05-26 15:28:59 +0100

    Created a function to find the midpoints of intervals in 'object.h' and 'object.cc' called 'Find_midpoints'. This avoid code duplication in 'Create_sphere' and 'Create_interf'

--- object.cc
+++ object.cc
@@ -14,11 +14,7 @@ void Create_sphere(particle sphere, double height, int n_int)
   sphere.height = height;
   sphere.n_int = n_int;
 
-  sphere.midpoints.resize(n_int); 
-  for (int i = 0; i < n_int; i++)
-    {
-      sphere.midpoints[i] = i * PI / (n_int - 1);
-    }
+  Find_midpoints(sphere.midpoints, 0.0, PI, n_int);
 }
 
 //Function to fill the properties of the surf structure
@@ -26,9 +22,16 @@ void Create_interf(surf interf, int n_int, double max_arc)
 {
   interf.n_int = n_int;
 
-  interf.midpoints.resize(n_int); 
+  Find_midpoints(interf.midpoints, 0.0, max_arc, n_int);
+}
+
+//Function to find the midpoints in a set of n_int equally spaced intervals
+void Find_midpoints(vector<double> midpoints, double start, double end, int n_int)
+{
+  midpoints.resize(n_int); 
   for (int i = 0; i < n_int; i++)
     {
-      interf.midpoints[i] = i * max_arc / (n_int - 1);
+      midpoints[i] = start + i * (end - start) / (n_int - 1);
     }
+
 }
--- object.h
+++ object.h
@@ -29,5 +29,8 @@ void Create_sphere(particle sphere, double height, int n_int);
 //Function to fill the properties of the surf structure
 void Create_interf(surf interf, int n_int, double max_arc);
 
+//Function to find the midpoints in a set of n_int equally spaced intervals
+void Find_midpoints(vector<double> midpoints, double start, double end, int n_int);
+
 #endif /* OBJECT_H */
 
