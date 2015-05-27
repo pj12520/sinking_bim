ede47ad5c53626698f0b127a20479052429d7b92
pj12520 <paul.jarvis@bristol.ac.uk>
2015-05-27 15:47:19 +0100

    Added functions to evaluate a1, a2 etc. as defined in the notes

--- axisym.cc
+++ axisym.cc
@@ -6,6 +6,101 @@ using math_const::PI;
 
 using namespace ellip_poly;
 
+//Function to calculate the quantity a1 as defined in the notes
+double A1(double viscos_rat, double sum_3_2, double diff, double beta_4, double source_rad, double alpha_2, double alpha_4, double source_rad_2, double pos_rad_2, double pos_rad, double beta_2)
+{
+  double a1 = ((1.0 - viscos_rat) / (PI * sum_3_2 * diff * beta_4)) * (source_rad * alpha_2 * (4.0 * alpha_4 - 18.0 * source_rad_2 * pos_rad_2) - (2.0 * pos_rad_2 + source_rad_2) * source_rad * (2.0 * alpha_4 - 3.0 * beta_4) - (pos_rad_2 + 2.0 * source_rad_2) * pos_rad * alpha_2 * beta_2 + source_rad * pos_rad_2 * beta_4);
+
+  return a1;
+}
+
+//Function to calculate the quantity a2 as defined in the notes
+double A2(double viscos_rat, double vert_diff, double alpha_4, double beta_4, double alpha_2, double vert_diff_2, double sum_3_2, double diff, double beta_2)
+{
+  double a2 = (1.0 - viscos_rat) * vert_diff * (2.0 * alpha_4 - 2.0 * beta_4 - alpha_2 * vert_diff_2) / (PI * sum_3_2 * diff * beta_2);
+
+  return a2;
+}
+
+//Function to calculate the quantity a3 as defined in the notes
+double A3(double viscos_rat, double sum_3_2, double diff_2, double beta_4, double source_rad, double alpha_8, double alpha_4, double beta_8, double pos_rad_2, double source_rad_2, double alpha_2, double pos_rad, double beta_2)
+{
+  double a3 = ((1.0 - viscos_rat) / (PI * sum_3_2 * diff_2 * beta_4)) * ((source_rad / 2) * (-8.0 * alpha_8 + 15.0 * alpha_4 * beta_4 - 3.0 * beta_8) - 2.0 * (2.0 * pos_rad_2 + source_rad_2) * source_rad * alpha_2 * (-alpha_4 + 3.0 * beta_4) + (pos_rad_2 + 2.0 * source_rad_2) * pos_rad * beta_2 * (alpha_4 + 3.0 * beta_4) - 4.0 * source_rad * pos_rad_2 * alpha_2 * beta_4);
+
+  return a3;
+}
+//Function to calculate the quantity a4 as defined in the notes
+double A4(double viscos_rat, double vert_diff, double beta_2, double alpha_2, double alpha_4, double beta_4, double vert_diff_2, double sum_3_2, double diff)
+{
+  double a4 = - (1.0 - viscos_rat) * vert_diff * beta_2 * (-alpha_2 * (alpha_4 - 5.0 * beta_4) - (alpha_2 - vert_diff_2)* (alpha_4 + 3.0 * beta_4)) / (PI * sum_3_2 * diff * beta_4);
+
+  return a4;
+}
+
+//Function to calculate the quantity a6 as defined in the notes
+double A6(double viscos_rat, double vert_diff_2, double source_rad_2, double alpha_2, double sum_3_2, double diff, double source_rad)
+{
+  double a6 = (1.0 - viscos_rat) * vert_diff_2 * (2.0 * source_rad_2 - alpha_2) / (2.0 * PI * sum_3_2 * diff * source_rad);
+
+  return a6;
+}
+
+//Function to calculate the quantity a8 as defined in the notes
+double A8(double viscos_rat, double vert_diff_2, double alpha_4, double beta_4, double source_rad_2, double alpha_2, double sum_3_2, double diff, double source_rad)
+{
+  double a8 = (1.0 - viscos_rat) * vert_diff_2 * (alpha_4 + 3.0 * beta_4 - 8.0 * source_rad_2 * alpha_2) / (2.0 * PI * sum_3_2 * diff * source_rad);
+
+  return a8;
+}
+
+//Function to calculate the quantity a9 as defined in the notes
+double A9(double viscos_rat, double vert_diff, double alpha_4, double beta_4, double pos_rad_2, double alpha_2, double pos_rad_4, double sum_3_2, double diff)
+{
+  double a9 = (1.0 - viscos_rat) * vert_diff * (-2.0 * alpha_4 + 3.0 * beta_4 - 4.0 * pos_rad_2 * alpha_2 + 4.0 * pos_rad_4) / (4.0 * PI * sum_3_2 * diff * pos_rad_2);
+
+  return a9;
+}
+
+//Function to calculate the quantity a10 as defined in the notes
+double A10(double viscos_rat, double vert_diff_2, double alpha_2, double pos_rad_2, double sum_3_2, double diff, double pos_rad)
+{
+  double a10 = (1.0 - viscos_rat) * vert_diff_2 * (alpha_2 - 2.0 * pos_rad_2) / (2.0 * PI * sum_3_2 * diff * pos_rad);
+
+  return a10;
+}
+
+//Function to calculate the quantity a11 as defined in the notes
+double A11(double viscos_rat, double vert_diff, double alpha_6, double alpha_2, double beta_4, double pos_rad_2, double pos_rad_4, double sum_3_2, double diff_2, double alpha_4)
+{
+  double a11 = (1.0 - viscos_rat) * vert_diff * (alpha_6 - 3.0 * alpha_2 * beta_4 + 2.0 * pos_rad_2 * alpha_4 + 6.0 * pos_rad_2 * beta_4 - 8.0 * pos_rad_4 * alpha_2) / (2.0 * PI * sum_3_2 * diff_2 * pos_rad_2);
+
+  return a11;
+}
+
+//Function to calculate the quantity a12 as defined in the notes
+double A12(double viscos_rat, double vert_diff_2, double pos_rad_2, double alpha_2, double alpha_4, double beta_4, double sum_3_2, double diff_2, double pos_rad)
+{
+  double a12 = (1.0 - viscos_rat) * vert_diff_2 * (8.0 * pos_rad_2 * alpha_2 - alpha_4 - 3.0 * beta_4) / (2.0 * PI * sum_3_2 * diff_2 * pos_rad);
+
+  return a12;
+}
+
+//Function to calculate the quantity a14 as defined in the notes
+double A14(double viscos_rat, double vert_diff_3, double sum_3_2, double diff)
+{
+  double a14 = (1.0 - viscos_rat) * vert_diff_3 / (PI * sum_3_2 * diff);
+
+  return a14;
+}
+
+//Function to calculate the quantity a16 as defined in the notes
+double A16(double viscos_rat, double alpha_2, double sum_3_2, double diff_2, double vert_diff_3)
+{
+  double a16 = -4.0 * (1.0 - viscos_rat) * vert_diff_3 * alpha_2 / (PI * sum_3_2 * diff_2);
+
+  return a16;
+}
+
 //Function to calculate the 11 component of B for an intermediate and regular interval
 double Matrix_B11(double beta_2, double sum_half, double alpha_2, double vert_diff_2, double sum, double diff, double ellip1, double ellip2)
 {
--- axisym.h
+++ axisym.h
@@ -4,6 +4,42 @@
 #define __AXISYM_H__
 #pragma once
 
+//Function to calculate the quantity a1 as defined in the notes
+double A1(double viscos_rat, double sum_3_2, double diff, double beta_4, double source_rad, double alpha_2, double alpha_4, double source_rad_2, double pos_rad_2, double pos_rad, double beta_2);
+
+//Function to calculate the quantity a2 as defined in the notes
+double A2(double viscos_rat, double vert_diff, double alpha_4, double beta_4, double alpha_2, double vert_diff_2, double sum_3_2, double diff, double beta_2);
+
+//Function to calculate the quantity a3 as defined in the notes
+double A3(double viscos_rat, double sum_3_2, double diff_2, double beta_4, double source_rad, double alpha_8, double alpha_4, double beta_8, double pos_rad_2, double source_rad_2, double alpha_2, double pos_rad, double beta_2);
+
+//Function to calculate the quantity a1 as defined in the notes
+double A4(double viscos_rat, double vert_diff, double beta_2, double alpha_2, double alpha_4, double beta_4, double vert_diff_2, double sum_3_2, double diff);
+
+//Function to calculate the quantity a6 as defined in the notes
+double A6(double viscos_rat, double vert_diff_2, double source_rad_2, double alpha_2, double sum_3_2, double diff, double source_rad);
+
+//Function to calculate the quantity a8 as defined in the notes
+double A8(double viscos_rat, double vert_diff_2, double alpha_4, double beta_4, double source_rad_2, double alpha_2, double sum_3_2, double diff, double source_rad);
+
+//Function to calculate the quantity a9 as defined in the notes
+double A9(double viscos_rat, double vert_diff, double alpha_4, double beta_4, double pos_rad_2, double alpha_2, double pos_rad_4, double sum_3_2, double diff);
+
+//Function to calculate the quantity a10 as defined in the notes
+double A10(double viscos_rat, double vert_diff_2, double alpha_2, double pos_rad_2, double sum_3_2, double diff, double pos_rad);
+
+//Function to calculate the quantity a11 as defined in the notes
+double A11(double viscos_rat, double vert_diff, double alpha_6, double alpha_2, double beta_4, double pos_rad_2, double pos_rad_4, double sum_3_2, double diff_2, double alpha_4);
+
+//Function to calculate the quantity a12 as defined in the notes
+double A12(double viscos_rat, double vert_diff_2, double pos_rad_2, double alpha_2, double alpha_4, double beta_4, double sum_3_2, double diff_2, double pos_rad);
+
+//Function to calculate the quantity a14 as defined in the notes
+double A14(double viscos_rat, double vert_diff_3, double sum_3_2, double diff);
+
+//Function to calculate the quantity a16 as defined in the notes
+double A16(double viscos_rat, double alpha_2, double sum_3_2, double diff_2, double vert_diff_3);
+
 //Function to calculate the 11 component of B for an intermediate and regular interval
 double Matrix_B11(double beta_2, double sum_half, double alpha_2, double vert_diff_2, double sum, double diff, double ellip1, double ellip2);
 
