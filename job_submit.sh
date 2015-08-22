c8397937c82ad89dcac1d05a1682a01fc74a5b8e
pj12520 <paul.jarvis@bristol.ac.uk>
2015-08-22 14:36:51 +0100

    Data from param sweep 3

--- job_submit.sh
+++ job_submit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd new_data
+cd param_sweep3
 
 for i in $(ls -d */); do
 
@@ -14,9 +14,9 @@ for i in $(ls -d */); do
 
 	    cd ${k}
 
-	    export INPUT=${HOME}/sinking_bim/trunk/new_data/${i}/${j}/${k}/dimensionless_input.dat
+	    export INPUT=${HOME}/sinking_bim/trunk/param_sweep3/${i}/${j}/${k}/dimensionless_input.dat
 
-	    export RUNDIR=${HOME}/sinking_bim/trunk/new_data/${i}/${j}/${k}
+	    export RUNDIR=${HOME}/sinking_bim/trunk/param_sweep3/${i}/${j}/${k}
 
 	    qsub -V ../../../../param_sweep.sh
 # echo $INPUT
